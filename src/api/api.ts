acd186a7f232b0a392d3ba823a293ef6620d2ba7
houssinCarnelian <houssin.carnelian@gmail.com>
2025-04-23 19:26:04 +0100

    first commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4258/api",
+  // baseURL: "http://localhost:4258/api",
+  baseURL: "https://trace-app-api-zkjn.onrender.com/api",
 });
-export const registerUser = async (userData:any) => {
-    const response = await API.post('/auth/register', userData);
-    return response.data;
-  };
-  
-  export const loginUser = async (userData:any) => {
-    const response = await API.post('/auth/login', userData);
-    return response.data;
-  };
+export const registerUser = async (userData: any) => {
+  const response = await API.post("/auth/register", userData);
+  return response.data;
+};
+
+export const loginUser = async (userData: any) => {
+  const response = await API.post("/auth/login", userData);
+  return response.data;
+};
 export default API;
